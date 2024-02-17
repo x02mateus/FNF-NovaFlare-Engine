cdbf817cd058571a3e0fb0f5a5efc63a55ebccfd
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-02-18 00:37:03 +0800

    Add files via upload

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -21,7 +21,7 @@ import states.TitleState;
 	public var opponentCodeFix:Bool = false;
 	public var botOpponentFix:Bool = true;
 	public var HealthDrainOPPOMult:Float = 0.5;
-	public var HealthDrainOPPO:Bool = true;	
+	public var HealthDrainOPPO:Bool = false;	
 	
 	public var showFPS:Bool = true;
 	public var showMS:Bool = true;
@@ -44,8 +44,22 @@ import states.TitleState;
 	public var splashAlpha:Float = 0.6;
 	public var oldHealthBarVersion:Bool = false;
 	public var opponentStrums:Bool = true;
+	public var camZooms:Bool = true;
+	public var judgementCounter:Bool = true;
+	public var hideHud:Bool = false;	
+	public var timeBarType:String = 'Time Left';
+	public var scoreZoom:Bool = true;
+	public var healthBarAlpha:Float = 1;
+	public var hitsoundVolume:Float = 0;
+	public var pauseMusic:String = 'Tea Time';
+	public var comboStacking:Bool = true;
+	public var showComboNum:Bool = true;
+	public var comboColor:Bool = true;	
+	public var comboOffsetFix:Bool = false;
+	public var showRating:Bool = true;	
 	
 	public var lowQuality:Bool = false;
+	public var gameQuality:Int = #if mobile 0 #else 1 #end;
 	public var flashing:Bool = true;
 	public var shaders:Bool = true;
 	public var colorblindMode:Int = 0;
@@ -57,60 +71,10 @@ import states.TitleState;
 	public var CustomFadeSound:Float = 0.5;
 	public var CustomFadeText:Bool = true;
 	
-	public var gameOverVibration:Bool = false;
-	
+	public var gameOverVibration:Bool = false;	
 	public var discordRPC:Bool = true;
-	public var camZooms:Bool = true;
-	public var judgementCounter:Bool = true;
-	public var hideHud:Bool = false;	
-	
-	public var arrowRGB:Array<Array<FlxColor>> = [
-		[0xFFC24B99, 0xFFFFFFFF, 0xFF3C1F56],
-		[0xFF00FFFF, 0xFFFFFFFF, 0xFF1542B7],
-		[0xFF12FA05, 0xFFFFFFFF, 0xFF0A4447],
-		[0xFFF9393F, 0xFFFFFFFF, 0xFF651038]];
-		
-	public var arrowRGBPixel:Array<Array<FlxColor>> = [
-		[0xFFE276FF, 0xFFFFF9FF, 0xFF60008D],
-		[0xFF3DCAFF, 0xFFF4FFFF, 0xFF003060],
-		[0xFF71E300, 0xFFF6FFE6, 0xFF003100],
-		[0xFFFF884E, 0xFFFFFAF5, 0xFF6C0000]];
-
-	
-	public var timeBarType:String = 'Time Left';
-	public var scoreZoom:Bool = true;
-	
-	public var healthBarAlpha:Float = 1;
-	public var hitsoundVolume:Float = 0;
-	public var pauseMusic:String = 'Tea Time';
 	public var checkForUpdates:Bool = false;
 	public var oldHscriptVersion:Bool = false;
-	public var comboStacking:Bool = true;
-	public var showComboNum:Bool = true;
-	public var showRating:Bool = true;
-	public var comboColor:Bool = true;	
-	
-	public var gameplaySettings:Map<String, Dynamic> = [
-		'scrollspeed' => 1.0,
-		'scrolltype' => 'multiplicative', 
-		// anyone reading this, amod is multiplicative speed mod, cmod is constant speed mod, and xmod is bpm based speed mod.
-		// an amod example would be chartSpeed * multiplier
-		// cmod would just be constantSpeed = chartSpeed
-		// and xmod basically works by basing the speed on the bpm.
-		// iirc (beatsPerSecond * (conductorToNoteDifference / 1000)) * noteSize (110 or something like that depending on it, prolly just use note.height)
-		// bps is calculated by bpm / 60
-		// oh yeah and you'd have to actually convert the difference to seconds which I already do, because this is based on beats and stuff. but it should work
-		// just fine. but I wont implement it because I don't know how you handle sustains and other stuff like that.
-		// oh yeah when you calculate the bps divide it by the songSpeed or rate because it wont scroll correctly when speeds exist.
-		// -kade
-		'songspeed' => 1.0,
-		'healthgain' => 1.0,
-		'healthloss' => 1.0,
-		'instakill' => false,
-		'practice' => false,
-		'botplay' => false,
-		'opponentplay' => false
-	];
 	
 	public var marvelousRating:Bool = true;	
 	public var marvelousSprite:Bool = true;	
@@ -138,6 +102,40 @@ import states.TitleState;
 	public var extraKeyReturn4:String = 'SPACE';
 	
 	public var language:Int = 0; //for english at begin
+	
+	public var arrowRGB:Array<Array<FlxColor>> = [
+		[0xFFC24B99, 0xFFFFFFFF, 0xFF3C1F56],
+		[0xFF00FFFF, 0xFFFFFFFF, 0xFF1542B7],
+		[0xFF12FA05, 0xFFFFFFFF, 0xFF0A4447],
+		[0xFFF9393F, 0xFFFFFFFF, 0xFF651038]];
+		
+	public var arrowRGBPixel:Array<Array<FlxColor>> = [
+		[0xFFE276FF, 0xFFFFF9FF, 0xFF60008D],
+		[0xFF3DCAFF, 0xFFF4FFFF, 0xFF003060],
+		[0xFF71E300, 0xFFF6FFE6, 0xFF003100],
+		[0xFFFF884E, 0xFFFFFAF5, 0xFF6C0000]];	
+	
+	public var gameplaySettings:Map<String, Dynamic> = [
+		'scrollspeed' => 1.0,
+		'scrolltype' => 'multiplicative', 
+		// anyone reading this, amod is multiplicative speed mod, cmod is constant speed mod, and xmod is bpm based speed mod.
+		// an amod example would be chartSpeed * multiplier
+		// cmod would just be constantSpeed = chartSpeed
+		// and xmod basically works by basing the speed on the bpm.
+		// iirc (beatsPerSecond * (conductorToNoteDifference / 1000)) * noteSize (110 or something like that depending on it, prolly just use note.height)
+		// bps is calculated by bpm / 60
+		// oh yeah and you'd have to actually convert the difference to seconds which I already do, because this is based on beats and stuff. but it should work
+		// just fine. but I wont implement it because I don't know how you handle sustains and other stuff like that.
+		// oh yeah when you calculate the bps divide it by the songSpeed or rate because it wont scroll correctly when speeds exist.
+		// -kade
+		'songspeed' => 1.0,
+		'healthgain' => 1.0,
+		'healthloss' => 1.0,
+		'instakill' => false,
+		'practice' => false,
+		'botplay' => false,
+		'opponentplay' => false
+	];
 }
 
 class ClientPrefs {
