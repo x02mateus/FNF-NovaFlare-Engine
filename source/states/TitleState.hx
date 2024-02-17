44d595b62cf1b9ca0acaee4180013feb79c3146e
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-02-17 21:22:07 +0800

    Add files via upload

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -105,8 +105,8 @@ class TitleState extends MusicBeatState
 		
 		#if mobile
 		if(!CopyState.checkExistingFiles() && !ignoreCopy)
-		    FlxTransitionableState.skipNextTransIn = false;
-		    FlxTransitionableState.skipNextTransOut = false;
+		    FlxTransitionableState.skipNextTransIn = true;
+		    FlxTransitionableState.skipNextTransOut = true;
 			FlxG.switchState(new CopyState());
 		#end
 
