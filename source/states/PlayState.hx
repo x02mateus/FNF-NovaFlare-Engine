22a1f3801b4e1d230f75d071e94769bf44f0648c
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-03-03 01:37:02 +0800

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -445,7 +445,7 @@ class PlayState extends MusicBeatState
     public var hscriptNum:Int = 0;
     public var hscriptLoadArray:Array<String> = [];
     
-	public function cacheCreate():Void
+	public function cacheCreate():dynamic
 	{
 		switch (curStage)
 		{
