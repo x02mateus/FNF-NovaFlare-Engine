c64e9d4f67502832c25194d93874d068d4e2db32
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-04-18 00:08:57 +0800

    Add files via upload

--- source/states/LoadingState.hx
+++ source/states/LoadingState.hx
@@ -133,7 +133,7 @@ class LoadingState extends MusicBeatState
 
 		if (!transitioning)
 		{
-			if (canChangeState && !finishedLoading && checkLoaded())
+			if (canChangeState && !finishedLoading && checkLoaded() && intendedPercent > 0.99)
 			{
 				transitioning = true;
 				onLoad();
@@ -218,7 +218,7 @@ class LoadingState extends MusicBeatState
 		#end
 	}
 	
-	var finishedLoading:Bool = false;
+	var finishedLoading:Bool = false; //use for stop update
 	function onLoad()
 	{
 		if (stopMusic && FlxG.sound.music != null)
@@ -344,8 +344,8 @@ class LoadingState extends MusicBeatState
 		dontPreloadDefaultVoices = false;
 		preloadCharacter(player1, prefixVocals);
 		if (player2 != player1) preloadCharacter(player2, prefixVocals);
-		if (!stageData.hide_girlfriend && gfVersion != player2 && gfVersion != player1)
-			preloadCharacter(gfVersion);
+		if (!stageData.hide_girlfriend && gfVersion != player2 && gfVersion != player1) preloadCharacter(gfVersion);
+			
 		preloadScript();
 		
 		if (!dontPreloadDefaultVoices && needsVoices) songsToPrepare.push(prefixVocals);
@@ -576,5 +576,153 @@ class LoadingState extends MusicBeatState
     	    imagesToPrepare.push(result);
     	    input = regex.matchedRight();
     	}				
+	}
+	
+	
+	var noteData:Array<SwagSection>;
+
+		// NEW SHIT
+		noteData = songData.notes;
+
+		var file:String = Paths.json(songName + '/events');
+		#if MODS_ALLOWED
+		if (FileSystem.exists(Paths.modsJson(songName + '/events')) || FileSystem.exists(file))
+		#else
+		if (Assets.exists(file))
+		#end
+		{
+			var eventsData:Array<Dynamic> = Song.loadFromJson('events', songName).events;
+			for (event in eventsData) //Event Notes
+				for (i in 0...event[1].length)
+					makeEvent(event, i);
+		}
+
+		for (section in noteData)
+		{
+			for (songNotes in section.sectionNotes)
+			{
+				var daStrumTime:Float = songNotes[0];
+        		var daNoteData:Int = Std.int(songNotes[1] % 4);
+        		var gottaHitNote:Bool = section.mustHitSection;
+        		
+        		if (ClientPrefs.data.filpChart) {
+        		    if (daNoteData == 0) {
+        		        daNoteData = 3;
+        		    }    
+        		    else if (daNoteData == 1) {
+        		        daNoteData = 2;
+        		    }    
+        		    else if (daNoteData == 2) {
+        		        daNoteData = 1;
+        		    }   
+        		    else if (daNoteData == 3) {
+        		        daNoteData = 0;
+        		    } 
+        		}
+        
+        		if (songNotes[1] > 3)
+        		{
+        			gottaHitNote = !section.mustHitSection;
+        		}
+        
+        		var oldNote:Note;
+        		if (unspawnNotes.length > 0)
+        			oldNote = unspawnNotes[Std.int(unspawnNotes.length - 1)];
+        		else
+        			oldNote = null;
+        
+        		var swagNote:Note = new Note(daStrumTime, daNoteData, oldNote);
+        		swagNote.mustPress = gottaHitNote;
+        		swagNote.sustainLength = songNotes[2];
+        		swagNote.gfNote = (section.gfSection && (songNotes[1]<4));
+        		swagNote.noteType = songNotes[3];
+        		if(!Std.isOfType(songNotes[3], String)) swagNote.noteType = ChartingState.noteTypeList[songNotes[3]]; //Backward compatibility + compatibility with Week 7 charts
+        
+        		swagNote.scrollFactor.set();
+        
+        		unspawnNotes.push(swagNote);
+        
+        		final susLength:Float = swagNote.sustainLength / Conductor.stepCrochet;
+        		final floorSus:Int = Math.floor(susLength) - ClientPrefs.data.fixLNL;
+        
+        		if(floorSus > 0) {
+        			for (susNote in 0...floorSus + 1)
+        			{
+        				oldNote = unspawnNotes[Std.int(unspawnNotes.length - 1)];
+        
+        				var sustainNote:Note = new Note(daStrumTime + (Conductor.stepCrochet * susNote), daNoteData, oldNote, true);
+        				sustainNote.mustPress = gottaHitNote;
+        				sustainNote.gfNote = (section.gfSection && (songNotes[1]<4));
+        				sustainNote.noteType = swagNote.noteType;
+        				sustainNote.scrollFactor.set();
+        				sustainNote.parent = swagNote;
+        				sustainNote.hitMultUpdate(susNote, floorSus + 1);
+        				unspawnNotes.push(sustainNote);
+        				swagNote.tail.push(sustainNote);
+        
+        				sustainNote.correctionOffset = swagNote.height / 2;
+        				if(!PlayState.isPixelStage)
+        				{
+        					if(oldNote.isSustainNote)
+        					{
+        						oldNote.scale.y *= Note.SUSTAIN_SIZE / oldNote.frameHeight;
+        						oldNote.scale.y /= playbackRate;
+        						oldNote.updateHitbox();
+        					}
+        
+        					if(ClientPrefs.data.downScroll)
+        						sustainNote.correctionOffset = 0;
+        				}
+        				else if(oldNote.isSustainNote)
+        				{
+        					oldNote.scale.y /= playbackRate;
+        					oldNote.updateHitbox();
+        				}
+        
+        				if (sustainNote.mustPress) sustainNote.x += FlxG.width / 2; // general offset
+        				else if(ClientPrefs.data.middleScroll)
+        				{
+        					sustainNote.x += 310;
+        					if(daNoteData > 1) //Up and Right
+        						sustainNote.x += FlxG.width / 2 + 25;
+        				}
+        			}
+        		}
+        
+        		if (swagNote.mustPress)
+        		{
+        			swagNote.x += FlxG.width / 2; // general offset
+        		}
+        		else if(ClientPrefs.data.middleScroll)
+        		{
+        			swagNote.x += 310;
+        			if(daNoteData > 1) //Up and Right
+        			{
+        				swagNote.x += FlxG.width / 2 + 25;
+        			}
+        		}
+        
+        		if(!noteTypes.contains(swagNote.noteType)) {
+        			noteTypes.push(swagNote.noteType);
+        		}	
+			}
+		}
+		for (event in songData.events) //Event Notes
+			for (i in 0...event[1].length)
+				makeEvent(event, i);
+
+		unspawnNotes.sort(sortByTime);
+		generatedMusic = true;
+	}
+
+	// called only once per different event (Used for precaching)
+	function eventPushed(event:EventNote) {
+		eventPushedUnique(event);
+		if(eventsPushed.contains(event.event)) {
+			return;
+		}
+
+		stagesFunc(function(stage:BaseStage) stage.eventPushed(event));
+		eventsPushed.push(event.event);
 	}
 }
