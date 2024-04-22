bbcefa081bbdc86f06a5e831410116817bca705b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-04-23 00:29:58 +0800

    Add files via upload

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -74,10 +74,10 @@ class StrumNote extends FlxSprite
 
 		if(PlayState.isPixelStage)
 		{
-			loadGraphic(Paths.image('pixelUI/' + texture));
+			loadGraphic(Paths.image(texture));
 			width = width / 4;
 			height = height / 5;
-			loadGraphic(Paths.image('pixelUI/' + texture), true, Math.floor(width), Math.floor(height));
+			loadGraphic(Paths.image(texture), true, Math.floor(width), Math.floor(height));
 
 			antialiasing = false;
 			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
