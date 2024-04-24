af0637f44a8a4c99150ace11db7ddd8d2dbdf786
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2024-04-24 21:08:03 +0800

    更新 FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -1355,7 +1355,7 @@ class FreeplayState extends MusicBeatState {
 			
 		if (FlxG.mouse.pressed && holdOptions && !searching && !listening && optionsGroup[curHoldOptions] != null) {
 			if (optionsGroup[curHoldOptions].alpha <= 1)
-				optionsGroup[curHoldOptions].alpha += elapsed;
+				optionsGroup[curHoldOptions].alpha += elapsed*3;
 				
 			if (optionsGroup[curHoldOptions].alpha > 1)
 				optionsGroup[curHoldOptions].alpha = 1;
