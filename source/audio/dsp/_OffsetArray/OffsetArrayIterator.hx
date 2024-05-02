d94f894376f1f2b972720cd3906868fb54599e00
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-05-03 00:24:11 +0800

    官方源码使用

---
+++ source/audio/dsp/_OffsetArray/OffsetArrayIterator.hx
@@ -0,0 +1,13 @@
+package audio.visualize.dsp._OffsetArray;
+
+class OffsetArrayIterator {
+	public var array;
+	public var offset;
+	public var enumeration;
+
+	public function new(array, offset) {
+		this.array = array;
+		this.offset = offset;
+		this.enumeration = 0;
+	}
+}
