1d06c74d082efab3051686fcfc39c6ed296c9563
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-05-03 10:37:02 +0800

    Add files via upload

--- source/objects/Note.hx
+++ source/objects/Note.hx
@@ -335,7 +335,7 @@ class Note extends FlxSprite
 
 		var skin:String = texture + postfix;
 		if(texture.length < 1) {
-			//skin = PlayState.SONG != null ? PlayState.SONG.arrowSkin : null;
+			skin = PlayState.SONG != null ? PlayState.SONG.arrowSkin : null;
 			if(skin == null || skin.length < 1){
 				skin = defaultNoteSkin + postfix;
 				if(ClientPrefs.data.noteSkin == ClientPrefs.defaultData.noteSkin){ 
