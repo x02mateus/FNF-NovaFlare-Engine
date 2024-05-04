b1c5e32f4dda27ea4a689183cdc3a54fc5c10437
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-05-04 21:54:19 +0800

    Add files via upload

--- source/psychlua/ReflectionFunctions.hx
+++ source/psychlua/ReflectionFunctions.hx
@@ -41,7 +41,8 @@ class ReflectionFunctions
 			
 			if (MusicBeatState.instance.mobileControls != null 
 			&& myClass == 'flixel.FlxG' 
-			&& variable.indexOf('key') != -1){
+			&& variable.indexOf('key') != -1
+			&& FlxG.mouse.pressed){
 			    var check:Dynamic;
 			    check = specialKeyCheck(variable); //fuck you old lua 🙃
 			    if (check != null) return check;
