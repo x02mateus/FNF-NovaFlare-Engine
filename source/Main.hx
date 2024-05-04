b5f330c49fa765088ab36dd511a9dc0e3cfbc477
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-05-04 16:35:14 +0800

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -3,7 +3,7 @@ package;
 import objects.screen.Graphics;
 import objects.screen.FPS;
 
-import audio.flash.apps.MusicAnalyzer;
+import audio.flash.apps.SpectrumViewer;
 import flixel.graphics.FlxGraphic;
 import flixel.FlxGame;
 import flixel.FlxState;
@@ -46,7 +46,7 @@ class Main extends Sprite
 	public static var fpsVar:FPS;
 	public static var watermark:Watermark;
 	
-	public static var musicViewer:MusicAnalyzer; 
+	public static var musicViewer:SpectrumViewer; 
 
 	#if mobile
 	public static final platform:String = "Phones";
@@ -180,7 +180,7 @@ class Main extends Sprite
 			watermark.visible = ClientPrefs.data.showWatermark;
 		}
 		
-		musicViewer = new MusicAnalyzer(); 
+		musicViewer = new SpectrumViewer(); 
 		addChild(musicViewer);
 
 		#if linux
