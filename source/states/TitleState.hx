132d2a2d7f6532e070d4eb2abc1266e6456a9879
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-05-16 22:19:20 +0800

    Add files via upload

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -137,28 +137,32 @@ class TitleState extends MusicBeatState
 		#end
 				
 		Mods.loadTopMod();
+		
+		Main.updateWatermark();
 
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.data.checkForUpdates && !closedState) {
-			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/beihu235/FNF-NovaFlare-Engine/main/gitVersion.txt");
-
-			http.onData = function (data:String)
-			{
-				updateVersion = data.split('\n')[0].trim();
-				var curVersion:String = MainMenuState.novaFlareEngineVersion.trim();
-				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
-				if(updateVersion != curVersion) {
-					trace('versions arent matching!');
-					mustUpdate = true;
-				}
-			}
-
-			http.onError = function (error) {
-				trace('error: $error');
-			}
-
-			http.request();
+		    try{
+    			trace('checking for update');
+    			var http = new haxe.Http("https://raw.githubusercontent.com/beihu235/FNF-NovaFlare-Engine/main/gitVersion.txt");
+    
+    			http.onData = function (data:String)
+    			{
+    				updateVersion = data.split('\n')[0].trim();
+    				var curVersion:String = MainMenuState.novaFlareEngineVersion.trim();
+    				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
+    				if(updateVersion != curVersion) {
+    					trace('versions arent matching!');
+    					mustUpdate = true;
+    				}
+    			}
+    
+    			http.onError = function (error) {
+    				trace('error: $error');
+    			}
+    
+    			http.request();
+			}			
 		}
 		#end				
 
