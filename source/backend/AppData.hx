4304128cd1848e1eabd0a730c5772555ecc55f02
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-27 03:48:19 +0800

    Add files via upload

--- source/backend/AppData.hx
+++ source/backend/AppData.hx
@@ -1,5 +1,7 @@
 package backend;
 
+// make Sscript can check those data
+
 #if android
 import android.os.AppDetails;
 
@@ -8,13 +10,17 @@ class AppData
     public static function getVersionName():String {
         return AppDetails.getVersionName();
     }
-
-    //public static function getPackageName():String {
-        //return AppDetails.getAppPackageName();
-    //}
-
+    
     public static function getVersionCode():Int {
         return AppDetails.getVersionCode();
+    }
+
+    public static function getPackageName():String {
+        return AppDetails.getPackageName();
+    }
+
+    public static function getAppName():String {        
+         return AppDetails.getAppName();
     }
 }
 #end
