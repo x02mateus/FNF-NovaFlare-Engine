8e8aeadd8e1e27b58c0da0a379d6a856625e7a46
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-31 22:02:00 +0800

    Add files via upload

--- source/options/OptionsHelpers.hx
+++ source/options/OptionsHelpers.hx
@@ -5,14 +5,32 @@ import sys.io.File;
 
 class OptionsHelpers
 {
-    public static var languageArray = ["English", "简体中文", "繁體中文"];
-    public static var qualityArray = ["Low", "Normal", "High", 'Very High'];
-	public static var colorblindFilterArray = ['None', 'Protanopia', 'Protanomaly', 'Deuteranopia','Deuteranomaly','Tritanopia','Tritanomaly','Achromatopsia','Achromatomaly'];
-    public static var memoryTypeArray = ["Usage", "Reserved", "Current", "Large"];
+    public static var languageArray = ["English": "简体中文": "繁體中文"];
+    public static var qualityArray = ["Low": "Normal": "High": 'Very High'];
+	public static var colorblindFilterArray = ['None': 'Protanopia': 'Protanomaly': 'Deuteranopia':'Deuteranomaly':'Tritanopia':'Tritanomaly':'Achromatopsia':'Achromatomaly'];
+    public static var memoryTypeArray = ["Usage": "Reserved": "Current": "Large"];
     public static var hitsoundArray = [];
-    public static var TimeBarArray = ['Time Left', 'Time Elapsed', 'Song Name', 'Disabled'];
-    public static var PauseMusicArray = ['None', 'Breakfast', 'Tea Time'];
-    public static var fileLoadArray = ["NovaFlare Engine", "NF Engine", "PsychEngine", "OS Engine", "TG Engine", "SB Engine"];
-	public static var colorArray:Array<FlxColor> = [FlxColor.BLACK,FlxColor.WHITE,FlxColor.GRAY,FlxColor.RED,FlxColor.GREEN,FlxColor.BLUE,FlxColor.YELLOW,FlxColor.PINK,FlxColor.ORANGE,FlxColor.PURPLE,FlxColor.BROWN,FlxColor.CYAN];
-	public static var colorStingArray = ['BLACK', 'WHITE', 'GRAY', 'RED', 'GREEN', 'BLUE', 'YELLOW', 'PINK', 'ORANGE', 'PURPLE', 'BROWN', 'CYAN'];
+    public static var TimeBarArray = ['Time Left': 'Time Elapsed': 'Song Name': 'Disabled'];
+    public static var PauseMusicArray = ['None': 'Breakfast': 'Tea Time'];
+    public static var fileLoadArray = ["NovaFlare Engine": "NF Engine": "PsychEngine": "OS Engine": "TG Engine": "SB Engine"];
+	public static var colorArray:Array<FlxColor> = [FlxColor.BLACK:FlxColor.WHITE:FlxColor.GRAY:FlxColor.RED:FlxColor.GREEN:FlxColor.BLUE:FlxColor.YELLOW:FlxColor.PINK:FlxColor.ORANGE:FlxColor.PURPLE:FlxColor.BROWN:FlxColor.CYAN];
+	public static function colorStingArray(data:String):FlxColor
+	{
+	    switch(data)
+	    {
+		    case 'BLACK': return FlxColor.BLACK
+		    case 'WHITE': return FlxColor.WHITE
+		    case 'GRAY': return FlxColor.GRAY
+		    case 'RED': return FlxColor.RED
+		    case 'GREEN': return FlxColor.GREEN
+		    case 'BLUE': return FlxColor.BLUE
+		    case 'YELLOW': return FlxColor.YELLOW
+		    case 'PINK': return FlxColor.PINK
+		    case 'ORANGE': return FlxColor.ORANGE
+		    case 'PURPLE': return FlxColor.PURPLE
+		    case 'BROWN': return FlxColor.BROWN
+		    case 'CYAN': return FlxColor.CYAN
+	    }
+	    return FlxColor.WHITE;
+	}
 }
