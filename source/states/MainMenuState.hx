5d0d1aede577010542b14499b3bf6551396f290b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-31 21:55:08 +0800

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -21,7 +21,7 @@ import openfl.Lib;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.7.3'; //This is also used for Discord RPC
-	public static var novaFlareEngineVersion:String = '1.1.4';
+	public static var novaFlareEngineVersion:String = '1.1.5';
 	public static var curSelected:Int = 0;
     public static var saveCurSelected:Int = 0;
     
