ea214406dd6167da8d5ef712eab7dec7cf3f3736
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-31 22:24:17 +0800

    Add files via upload

--- source/options/OptionsHelpers.hx
+++ source/options/OptionsHelpers.hx
@@ -9,18 +9,18 @@ class OptionsHelpers
 	{
 	    switch(data)
 	    {
-		    case 'BLACK': return FlxColor.BLACK
-		    case 'WHITE': return FlxColor.WHITE
-		    case 'GRAY': return FlxColor.GRAY
-		    case 'RED': return FlxColor.RED
-		    case 'GREEN': return FlxColor.GREEN
-		    case 'BLUE': return FlxColor.BLUE
-		    case 'YELLOW': return FlxColor.YELLOW
-		    case 'PINK': return FlxColor.PINK
-		    case 'ORANGE': return FlxColor.ORANGE
-		    case 'PURPLE': return FlxColor.PURPLE
-		    case 'BROWN': return FlxColor.BROWN
-		    case 'CYAN': return FlxColor.CYAN
+		    case 'BLACK': return FlxColor.BLACK;
+		    case 'WHITE': return FlxColor.WHITE;
+		    case 'GRAY': return FlxColor.GRAY;
+		    case 'RED': return FlxColor.RED;
+		    case 'GREEN': return FlxColor.GREEN;
+		    case 'BLUE': return FlxColor.BLUE;
+		    case 'YELLOW': return FlxColor.YELLOW;
+		    case 'PINK': return FlxColor.PINK;
+		    case 'ORANGE': return FlxColor.ORANGE;
+		    case 'PURPLE': return FlxColor.PURPLE;
+		    case 'BROWN': return FlxColor.BROWN;
+		    case 'CYAN': return FlxColor.CYAN;
 	    }
 	    return FlxColor.WHITE;
 	}
